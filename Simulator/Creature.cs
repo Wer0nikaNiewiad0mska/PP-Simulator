e7f2b7c914dd318b06807bae2a0fca7a67df9bce
Weronika Niewiadomska <weronika0426@onet.eu>
2024-10-28 19:34:05 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+internal class Creature
+{
+    private string? name;
+    public string? Name { get; set; }
+
+    private int level;
+    public int Level { get; set; } = 1;
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
+    public string Info => $"{Name} - {Level}";
+}
+
