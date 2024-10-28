9ec6fe6f8548bfa63e49658aafd5ec7fd2b77f53
Weronika Niewiadomska <weronika0426@onet.eu>
2024-10-28 20:56:54 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public required string Description 
+    { 
+        get => description; 
+        init
+        {
+            string trimmedDescription = value.Trim();
+            if (trimmedDescription.Length < 3)
+            {
+                trimmedDescription = trimmedDescription.PadRight(3, '#');
+            }
+            else if (trimmedDescription.Length > 15)
+            {
+                trimmedDescription = trimmedDescription.Substring(0, 15).TrimEnd();
+                if (trimmedDescription.Length < 3) trimmedDescription = trimmedDescription.PadRight(3, '#');
+            }
+            description = char.ToUpper(trimmedDescription[0]) + trimmedDescription.Substring(1);
+        }
+    }
     public uint Size { get; set; } = 3;
 
     public string Info => $"{Description} <{Size}>";
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -10,11 +10,50 @@ namespace Simulator;
 
 internal class Creature
 {
-    private string? name;
-    public string? Name { get; set; }
+    private string? name = "Unknown";
+    public string? Name
+    {
+        get => name;
+        init
+        {
+            string trimmedName = value.Trim();
+            if (trimmedName.Length < 3)
+            {
+                trimmedName = trimmedName.PadRight(3, '#');
+            }
+            else if (trimmedName.Length > 25) 
+            {
+                trimmedName = trimmedName.Substring(0, 25).TrimEnd();
+                if (trimmedName.Length < 3) trimmedName = trimmedName.PadRight(3, '#');
+            }
+            name = char.ToUpper(trimmedName[0]) + trimmedName.Substring(1);
+        }
+    }
 
-    private int level;
-    public int Level { get; set; } = 1;
+    private int level = 1;
+    public int Level 
+    { 
+        get => level;
+        init
+        {
+            if (level < 1)
+            {
+                level = 1;
+            }
+            else if (value > 10)
+            {
+                level = 10;
+            }
+        }
+    }
+
+    public void Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+        }
+    }
 
     public Creature(string name, int level = 1)
     {
