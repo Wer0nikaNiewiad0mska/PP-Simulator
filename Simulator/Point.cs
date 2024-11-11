32871962a37900b09918ce5ef52ad61c674a98c0
Weronika Niewiadomska <weronika0426@onet.eu>
2024-11-11 17:42:08 +0100

    Prostokąty

---
+++ Simulator/Point.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Simulator.Directions;
+
+namespace Simulator;
+
+public readonly struct Point
+{
+    public readonly int X, Y;
+    public Point(int x, int y) => (X, Y) = (x, y);
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point Next(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return new Point(X, Y + 1);
+            case Direction.Down:
+                return new Point(X, Y - 1);
+            case Direction.Left:
+                return new Point(X - 1, Y);
+            case Direction.Right:
+                return new Point(X + 1, Y);               
+        }
+        return default;
+    }
+
+    // rotate given direction 45 degrees clockwise
+    public Point NextDiagonal(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return new Point(X + 1, Y + 1);
+            case Direction.Down:
+                return new Point(X - 1, Y - 1);
+            case Direction.Left:
+                return new Point(X - 1, Y + 1);
+            case Direction.Right:
+                return new Point(X + 1, Y - 1);
+        }
+        return default;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using static Simulator.Directions;
+
 namespace Simulator;
 
 internal class Program
@@ -6,61 +8,41 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Lab4a();
-        Lab4b();
+        Lab5a();
     }
-    static void Lab4a()
+   
+    static void Lab5a()
     {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
+        try
         {
-            o.Hunt();
-            o.SayHi();
-        }
+            Rectangle r1 = new(4, 4, 2, 2);
+            Console.WriteLine(r1);
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
+            Rectangle r2 = new(2, 2, 4, 4);
+            Console.WriteLine(r2);
+
+            try
+            {
+                Rectangle r3 = new(3, 8, 3, 9);
+                Console.WriteLine(r3);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error occured: " + ex.Message);
+            }
+
+            Rectangle r4 = new(new Point(2, 2), new Point(6, 8));
+            Console.WriteLine(r4);
+
+            Point insidePoint = new(4, 4);
+            Point outsidePoint = new(7, 9);
+
+            Console.WriteLine($"Point {insidePoint} {(r4.Contains(insidePoint) ? "is" : "is not")} inside the rectangle {r4}");
+            Console.WriteLine($"Point {outsidePoint} {(r4.Contains(outsidePoint) ? "is" : "is not")} inside the rectangle {r4}");
+        }
+        catch (Exception ex)
         {
-            e.Sing();
-            e.SayHi();
+            Console.WriteLine("Error occured: " + ex.Message);
         }
-
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
-        {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-        }
-
-        Creature c = new Elf("Elandor", 5, 3);
-        Console.WriteLine(c);  // ELF: Elandor [5]
-    }
-
-    static void Lab4b()
-    {
-        object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-        Console.WriteLine("\nMy objects:");
-        foreach (var o in myObjects) Console.WriteLine(o);
-        /*
-            My objects:
-            ANIMALS: Dogs <3>
-            BIRDS: Eagles (fly+) <10>
-            ELF: E## [10][0]
-            ORC: Morgash [6][4]
-        */
     }
 }
